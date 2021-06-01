26545ff34c695a1ccd736eab78868b85fb5aba6f
Chinmay Tawde <chinmaytawde15@gmail.com>
2021-05-31 17:14:34 -0700

    Added secret_key generation class and gitignore

--- Generate_Key.py
+++ Generate_Key.py
@@ -1,18 +1,12 @@
 from cryptography.fernet import Fernet
 
-# def generate_key():
-#     """
-#     Generates a key and save it into a file
-#     """
-#     key = Fernet.generate_key()
-#     with open("secret.key", "wb") as key_file:
-#         key_file.write(key)
-#
-# #generate_key()
+def generate_key():
+    """
+    Generates a key and save it into a file
+    """
+    key = Fernet.generate_key()
+    with open("secret.key", "wb") as key_file:
+        key_file.write(key)
 
-from tkinter import *
-from tkinter import font
-root=Tk()
-fonts= font.families()
-for i in fonts:
-    print(i)
+if __name__ == "__main__":
+    generate_key()
